fe1829cd73d4d2c7bbcd9c7e1d40fb7fa2cbaf6b
Kian Woo <kianwoo@Kians-iMac.local>
2016-02-21 14:32:39 +1100

    Optimize images using image-min

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var imageResize = require('gulp-image-resize');
 var rename = require('gulp-rename');
- 
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
 gulp.task('default', ['copy-html', 'copy-css', 'copy-img', 'generate-thumbnail'], function() {
   // place code for your default task here
@@ -24,6 +25,11 @@ gulp.task('copy-css', function() {
 
 gulp.task('copy-img', function() {
 	gulp.src('./src/img/*' )
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
 		.pipe(gulp.dest('./dist/img'));
 });
 
@@ -33,6 +39,11 @@ gulp.task('generate-thumbnail', function() {
 		.pipe(rename('pizzeria-thumbnail.jpg'))
 		.pipe(imageResize({
 			width : 115
+		}))
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
 		}))
 		.pipe(gulp.dest('./dist/views/images'));
 });
