901b3cc348525d0a5fbb9d54fc431c90bf25a38e
Kian Woo <kian.woo@gmail.com>
2016-02-21 11:09:14 +1100

    Add copy-html task to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
+gulp.task('default', ['copy-html'], function() {
   // place code for your default task here
   browserSync.init({
-		server: './'
+		server: './dist'
 	});
 });
+
+
+gulp.task('copy-html', function() {
+	gulp.src('./src/*.html' )
+		.pipe(gulp.dest('./dist'));
+});
+
+
