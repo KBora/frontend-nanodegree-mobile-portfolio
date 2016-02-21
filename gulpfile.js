21e6d595cc37967a84bd219ef0068c783643b119
Kian Woo <kianwoo@Kians-iMac.local>
2016-02-21 12:44:05 +1100

    Create pizza thumbnail via gulp and reference it in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var imageResize = require('gulp-image-resize');
+var rename = require('gulp-rename');
+ 
 
-gulp.task('default', ['copy-html'], function() {
+gulp.task('default', ['copy-html', 'copy-css', 'copy-img', 'generate-thumbnail'], function() {
   // place code for your default task here
-  browserSync.init({
+	browserSync.init({
 		server: './dist'
 	});
 });
@@ -14,4 +17,22 @@ gulp.task('copy-html', function() {
 		.pipe(gulp.dest('./dist'));
 });
 
+gulp.task('copy-css', function() {
+	gulp.src('./src/css/*.css' )
+		.pipe(gulp.dest('./dist/css'));
+});
 
+gulp.task('copy-img', function() {
+	gulp.src('./src/img/*' )
+		.pipe(gulp.dest('./dist/img'));
+});
+
+
+gulp.task('generate-thumbnail', function() {
+	gulp.src('./src/views/images/pizzeria.jpg')
+		.pipe(rename('pizzeria-thumbnail.jpg'))
+		.pipe(imageResize({
+			width : 115
+		}))
+		.pipe(gulp.dest('./dist/views/images'));
+});
